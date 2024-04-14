a8b50d8e0766b12852f3c7acabffb0b8cbba1803
Yamana Pavan Kumar <79590309+Yamana06@users.noreply.github.com>
2024-04-15 01:38:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,24 +4,25 @@ import cv2
 import numpy as np
 import streamlit as st
 import torch
+import types  # Import the types module
 from fer_pytorch.fer import FER
 
-st.set_option("deprecation.showfileUploaderEncoding", False)
+def my_hash_func(func):
+    return hash((func.__code__, func.__name__))
 
-
-@st.cache
+@st.cache_data(hash_funcs={types.FunctionType: my_hash_func})  # Use st.cache_data instead of st.cache
 def cached_fer():
     fer = FER()
     fer.get_pretrained_model("resnet34")
     return fer
-
 
 def main():
     fer = cached_fer()
 
     st.title("Recognize emotions")
 
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    # Add a unique key to st.file_uploader to prevent DuplicateWidgetID error
+    uploaded_file = st.file_uploader("Choose an image...", type="jpg", key="unique_file_uploader")
 
     if uploaded_file is not None:
         file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
@@ -37,7 +38,6 @@ def main():
         else:
             print(output)
             st.image(image, caption="After", use_column_width=True, channels="BGR")
-
 
 if __name__ == "__main__":
     main()
